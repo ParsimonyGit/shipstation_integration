3a054f0d01b9b9df95585bf75595cb5ee7916d61
Rohan <Alchez@users.noreply.github.com>
2021-09-20 16:19:46 +0530

    fix: set the correct weight parameters based on item details (#35)

--- shipstation_integration/items.py
+++ shipstation_integration/items.py
@@ -1,12 +1,30 @@
+from typing import TYPE_CHECKING, Optional, Union
+
+from shipstation.models import ShipStationItem, ShipStationOrderItem
+
 import frappe
+from frappe.utils import flt
+
+if TYPE_CHECKING:
+	from shipstation_integration.shipstation_integration.doctype.shipstation_store.shipstation_store import (
+		ShipstationStore,
+	)
+	from shipstation_integration.shipstation_integration.doctype.shipstation_settings.shipstation_settings import (
+		ShipstationSettings,
+	)
 
 
-def create_item(product, settings, store=None):
+def create_item(
+	product: Union[ShipStationItem, ShipStationOrderItem],
+	settings: "ShipstationSettings",
+	store: Optional["ShipstationStore"] = None,
+):
 	"""
 	Create or update a Shipstation item, and setup item defaults.
 
 	Args:
-		product (shipstation.ShipStationItem): The Shipstation item.
+		product (shipstation.ShipStationItem | shipstation.ShipStationOrderItem):
+			The Shipstation item or order item document.
 		settings (ShipstationSettings, optional): A Shipstation Settings instance.
 		store (ShipstationStore, optional): The selected Shipstation store.
 			Defaults to None.
@@ -25,28 +43,54 @@ def create_item(product, settings, store=None):
 		item = frappe.get_doc("Item", item_code)
 	else:
 		item = frappe.new_doc("Item")
-		item.update({
-			"item_code": product.sku or product.name[:140],
-			"item_name": product.name[:140],
-			"item_group": settings.default_item_group,
-			"is_stock_item": 1,
-			"include_item_in_manufacturing": 0,
-			"description": getattr(product, "internal_notes", product.name),
-			"weight_per_unit": getattr(product, "weight_oz", 0),
-			"weight_uom": "Ounce",
-			"end_of_life": ""
-		})
+		item.update(
+			{
+				"item_code": product.sku or product.name[:140],
+				"item_name": product.name[:140],
+				"item_group": settings.default_item_group,
+				"is_stock_item": 1,
+				"include_item_in_manufacturing": 0,
+				"description": getattr(product, "internal_notes", product.name),
+				"end_of_life": "",
+			}
+		)
 
+	# update weight values
+	weight_per_unit, weight_uom = 1.0, "Ounce"
+	if isinstance(product, ShipStationItem):
+		weight_per_unit = flt(getattr(product, "weight_oz", 1))
+	elif isinstance(product, ShipStationOrderItem):
+		weight = product.weight if hasattr(product, "weight") else frappe._dict()
+		if weight:
+			weight_per_unit = flt(weight.value) if weight else 1
+			weight_uom = weight.units.title() if weight and weight.units else "Ounce"
+			if weight_uom == "Ounces":
+				# map Shipstation UOM to ERPNext UOM
+				weight_uom = "Ounce"
+
+	item.update(
+		{
+			"weight_per_unit": weight_per_unit,
+			"weight_uom": weight_uom,
+		}
+	)
+
+	# create item defaults, if missing
 	if store and store.company and not item.get("item_defaults"):
-		item.set("item_defaults", [{
-			"company": store.company,
-			"default_price_list": "ShipStation",
-			"default_warehouse": "",  # leave unset
-			"buying_cost_center": store.cost_center,
-			"selling_cost_center": store.cost_center,
-			"expense_account": store.expense_account,
-			"income_account": store.sales_account
-		}])
+		item.set(
+			"item_defaults",
+			[
+				{
+					"company": store.company,
+					"default_price_list": "ShipStation",
+					"default_warehouse": "",  # leave unset
+					"buying_cost_center": store.cost_center,
+					"selling_cost_center": store.cost_center,
+					"expense_account": store.expense_account,
+					"income_account": store.sales_account,
+				}
+			],
+		)
 
 	item.save()
 	return item.item_code

45fb26ec31c0cf123a9f3ddb8c86988592cf5260
Rohan Bansal <rohan@parsimony.com>
2021-10-08 15:47:00 +0530

    style: formatting

--- shipstation_integration/setup.py
+++ shipstation_integration/setup.py
@@ -6,25 +6,25 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 def get_setup_stages(args=None):
 	return [
 		{
-			'status': _('Creating shipstation masters'),
-			'fail_msg': _('Failed to create shipstation masters'),
-			'tasks': [
+			"status": _("Creating shipstation masters"),
+			"fail_msg": _("Failed to create shipstation masters"),
+			"tasks": [
 				{
-					'fn': create_customer_group,
-					'args': args,
-					'fail_msg': _("Failed to create Shipstation Customer Group")
+					"fn": create_customer_group,
+					"args": args,
+					"fail_msg": _("Failed to create Shipstation Customer Group"),
 				},
 				{
-					'fn': create_price_list,
-					'args': args,
-					'fail_msg': _("Failed to create Shipstation Price List")
+					"fn": create_price_list,
+					"args": args,
+					"fail_msg": _("Failed to create Shipstation Price List"),
 				},
 				{
-					'fn': setup_custom_fields,
-					'args': args,
-					'fail_msg': _("Failed to create Shipstation custom fields")
-				}
-			]
+					"fn": setup_custom_fields,
+					"args": args,
+					"fail_msg": _("Failed to create Shipstation custom fields"),
+				},
+			],
 		}
 	]
 
@@ -41,98 +41,222 @@ def setup_shipstation():
 
 
 def create_customer_group(args=None):
-	if frappe.db.get_value('Customer Group', {'customer_group_name': 'ShipStation'}):
+	if frappe.db.get_value("Customer Group", {"customer_group_name": "ShipStation"}):
 		return
 
 	customer_group = frappe.new_doc("Customer Group")
-	customer_group.customer_group_name = 'ShipStation'
-	customer_group.parent_customer_group = 'All Customer Groups'
+	customer_group.customer_group_name = "ShipStation"
+	customer_group.parent_customer_group = "All Customer Groups"
 	customer_group.save()
 
 
 def create_price_list(args=None):
-	if frappe.db.get_value('Price List', {'price_list_name': 'ShipStation'}):
+	if frappe.db.get_value("Price List", {"price_list_name": "ShipStation"}):
 		return
 
 	price_list = frappe.new_doc("Price List")
-	price_list.price_list_name = 'ShipStation'
+	price_list.price_list_name = "ShipStation"
 	price_list.selling = True
 	price_list.save()
 
 
 def setup_custom_fields(args=None):
 	warehouse_fields = [
-		dict(fieldtype="Data", fieldname="shipstation_warehouse_id", read_only=1,
-			label="Shipstation Warehouse ID", insert_after="parent_warehouse",
-			translatable=0)
+		dict(
+			fieldtype="Data",
+			fieldname="shipstation_warehouse_id",
+			read_only=1,
+			label="Shipstation Warehouse ID",
+			insert_after="parent_warehouse",
+			translatable=0,
+		)
 	]
 
 	common_custom_sales_fields = [
-		dict(fieldtype="Data", fieldname="shipstation_store_name", read_only=1,
-			label="Shipstation Store", insert_after="sb_shipstation", translatable=0),
-		dict(fieldtype="Data", fieldname="shipstation_order_id", read_only=1,
-			label="Shipstation Order ID", insert_after="shipstation_store_name",
-			in_standard_filter=1, translatable=0),
-		dict(fieldtype="Column Break", fieldname="cb_shipstation",
-			insert_after="shipstation_order_id"),
-		dict(fieldtype="Data", fieldname="marketplace", read_only=1,
-			label="Marketplace", insert_after="cb_shipstation", translatable=0),
-		dict(fieldtype="Data", fieldname="marketplace_order_id", read_only=1,
-			label="Marketplace Order ID", insert_after="marketplace",
-			translatable=0),
-		dict(fieldtype="Check", fieldname="has_pii",
-			hidden=1, label="Has PII", insert_after="marketplace_order_id"),
-		dict(fieldtype="Section Break", fieldname="sb_notes", collapsible=1,
-			label="Notes", insert_after="has_pii",
-			depends_on="eval:doc.shipstation_customer_notes || doc.shipstation_internal_notes"),
-		dict(fieldtype="Long Text", fieldname="shipstation_customer_notes", read_only=1,
-			label="Shipstation Customer Notes", insert_after="sb_notes",
-			translatable=0),
-		dict(fieldtype="Column Break", fieldname="cb_notes",
-			insert_after="shipstation_customer_notes"),
-		dict(fieldtype="Long Text", fieldname="shipstation_internal_notes", read_only=1,
-			label="Shipstation Internal Notes", insert_after="cb_notes", translatable=0),
+		dict(
+			fieldtype="Data",
+			fieldname="shipstation_store_name",
+			read_only=1,
+			label="Shipstation Store",
+			insert_after="sb_shipstation",
+			translatable=0,
+		),
+		dict(
+			fieldtype="Data",
+			fieldname="shipstation_order_id",
+			read_only=1,
+			label="Shipstation Order ID",
+			insert_after="shipstation_store_name",
+			in_standard_filter=1,
+			translatable=0,
+		),
+		dict(
+			fieldtype="Column Break",
+			fieldname="cb_shipstation",
+			insert_after="shipstation_order_id",
+		),
+		dict(
+			fieldtype="Data",
+			fieldname="marketplace",
+			read_only=1,
+			label="Marketplace",
+			insert_after="cb_shipstation",
+			translatable=0,
+		),
+		dict(
+			fieldtype="Data",
+			fieldname="marketplace_order_id",
+			read_only=1,
+			label="Marketplace Order ID",
+			insert_after="marketplace",
+			translatable=0,
+		),
+		dict(
+			fieldtype="Check",
+			fieldname="has_pii",
+			hidden=1,
+			label="Has PII",
+			insert_after="marketplace_order_id",
+		),
+		dict(
+			fieldtype="Section Break",
+			fieldname="sb_notes",
+			collapsible=1,
+			label="Notes",
+			insert_after="has_pii",
+			depends_on="eval:doc.shipstation_customer_notes || doc.shipstation_internal_notes",
+		),
+		dict(
+			fieldtype="Long Text",
+			fieldname="shipstation_customer_notes",
+			read_only=1,
+			label="Shipstation Customer Notes",
+			insert_after="sb_notes",
+			translatable=0,
+		),
+		dict(
+			fieldtype="Column Break",
+			fieldname="cb_notes",
+			insert_after="shipstation_customer_notes",
+		),
+		dict(
+			fieldtype="Long Text",
+			fieldname="shipstation_internal_notes",
+			read_only=1,
+			label="Shipstation Internal Notes",
+			insert_after="cb_notes",
+			translatable=0,
+		),
 	]
 
 	sales_order_fields = [
-		dict(fieldtype="Section Break", fieldname="sb_shipstation", collapsible=1,
-			label="Shipstation", insert_after="tax_id"),
+		dict(
+			fieldtype="Section Break",
+			fieldname="sb_shipstation",
+			collapsible=1,
+			label="Shipstation",
+			insert_after="tax_id",
+		),
 	] + common_custom_sales_fields
 
-	sales_invoice_fields = [
-		dict(fieldtype="Section Break", fieldname="sb_shipstation", collapsible=1,
-			label="Shipstation", insert_after="amended_from"),
-	] + common_custom_sales_fields + [
-		dict(fieldtype="Data", fieldname="shipstation_shipment_id", read_only=1,
-			label="Shipstation Shipment ID", insert_after="shipstation_order_id",
-			translatable=0)
-	]
+	sales_invoice_fields = (
+		[
+			dict(
+				fieldtype="Section Break",
+				fieldname="sb_shipstation",
+				collapsible=1,
+				label="Shipstation",
+				insert_after="amended_from",
+			),
+		]
+		+ common_custom_sales_fields
+		+ [
+			dict(
+				fieldtype="Data",
+				fieldname="shipstation_shipment_id",
+				read_only=1,
+				label="Shipstation Shipment ID",
+				insert_after="shipstation_order_id",
+				translatable=0,
+			)
+		]
+	)
 
-	delivery_note_fields = [
-		dict(fieldtype="Section Break", fieldname="sb_shipstation", collapsible=1,
-			label="Shipstation", insert_after="return_against"),
-	] + common_custom_sales_fields + [
-		dict(fieldtype="Data", fieldname="shipstation_shipment_id", read_only=1,
-			label="Shipstation Shipment ID", insert_after="shipstation_order_id",
-			translatable=0),
-		dict(fieldtype="Section Break", fieldname="sb_shipment", collapsible=1,
-			label="Shipment Details", insert_after="has_pii"),
-		dict(fieldtype="Data", fieldname="carrier", read_only=1,
-			label="Carrier", insert_after="sb_shipment", translatable=0),
-		dict(fieldtype="Data", fieldname="tracking_number", read_only=1,
-			label="Tracking Number", insert_after="carrier", translatable=0),
-		dict(fieldtype="Column Break", fieldname="cb_shipment",
-			insert_after="tracking_number"),
-		dict(fieldtype="Data", fieldname="carrier_service", read_only=1,
-			label="Carrier Service", insert_after="cb_shipment",
-			translatable=0)
-	]
+	delivery_note_fields = (
+		[
+			dict(
+				fieldtype="Section Break",
+				fieldname="sb_shipstation",
+				collapsible=1,
+				label="Shipstation",
+				insert_after="return_against",
+			),
+		]
+		+ common_custom_sales_fields
+		+ [
+			dict(
+				fieldtype="Data",
+				fieldname="shipstation_shipment_id",
+				read_only=1,
+				label="Shipstation Shipment ID",
+				insert_after="shipstation_order_id",
+				translatable=0,
+			),
+			dict(
+				fieldtype="Section Break",
+				fieldname="sb_shipment",
+				collapsible=1,
+				label="Shipment Details",
+				insert_after="has_pii",
+			),
+			dict(
+				fieldtype="Data",
+				fieldname="carrier",
+				read_only=1,
+				label="Carrier",
+				insert_after="sb_shipment",
+				translatable=0,
+			),
+			dict(
+				fieldtype="Data",
+				fieldname="tracking_number",
+				read_only=1,
+				label="Tracking Number",
+				insert_after="carrier",
+				translatable=0,
+			),
+			dict(
+				fieldtype="Column Break",
+				fieldname="cb_shipment",
+				insert_after="tracking_number",
+			),
+			dict(
+				fieldtype="Data",
+				fieldname="carrier_service",
+				read_only=1,
+				label="Carrier Service",
+				insert_after="cb_shipment",
+				translatable=0,
+			),
+		]
+	)
 
 	sales_order_item_fields = [
-		dict(fieldtype="Section Break", fieldname="sb_shipstation", collapsible=1,
-			label="Shipstation", insert_after="weight_uom"),
-		dict(fieldtype="Long Text", fieldname="shipstation_item_notes", read_only=1,
-			label="Shipstation Item Notes", insert_after="sb_shipstation", translatable=0),
+		dict(
+			fieldtype="Section Break",
+			fieldname="sb_shipstation",
+			collapsible=1,
+			label="Shipstation",
+			insert_after="weight_uom",
+		),
+		dict(
+			fieldtype="Long Text",
+			fieldname="shipstation_item_notes",
+			read_only=1,
+			label="Shipstation Item Notes",
+			insert_after="sb_shipstation",
+			translatable=0,
+		),
 	]
 
 	custom_fields = {
@@ -140,7 +264,7 @@ def setup_custom_fields(args=None):
 		"Sales Order": sales_order_fields,
 		"Sales Order Item": sales_order_item_fields,
 		"Sales Invoice": sales_invoice_fields,
-		"Delivery Note": delivery_note_fields
+		"Delivery Note": delivery_note_fields,
 	}
 
 	print("Creating custom fields for Shipstation")

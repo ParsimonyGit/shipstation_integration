43ba3f5c02f362c847b5e77830e30eb77477f8ee
Avunu LLC <mail@avu.nu>
2022-09-21 13:17:21 +0000

    cleanup

--- shipstation_integration/setup.py
+++ shipstation_integration/setup.py
@@ -298,4 +298,3 @@ def setup_custom_fields(args=None):
 	print("Creating custom fields for Shipstation")
 	create_custom_fields(custom_fields)
 
-# def update_item_custom_fields(order_item_custom_fields):
